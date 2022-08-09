f21bd444de8a154bb8de7ab008ecfcfcbba615d0
Federico Madoery <fede.madoery@hotmail.com>
2022-08-09 13:39:12 -0300

    Remove Backed by for now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -115,31 +115,6 @@ export default function Home() {
 
         <Divider sx={{ borderColor: '#66F5DF' }} />
 
-        <Box sx={{ marginTop: 4 }}>
-          <SectionTitle>Backed By</SectionTitle>
-          <Box
-            sx={{
-              display: 'flex',
-              flexFlow: 'row wrap',
-              columnGap: 3,
-              rowGap: 3,
-              marginBottom: 4,
-            }}
-          >
-            {Array.from({ length: 10 }).map((v, i) => {
-              return (
-                <Box
-                  sx={{
-                    backgroundColor: '#D9D9D9',
-                    height: 150,
-                    flex: '1 1 20%',
-                  }}
-                  key={i}
-                />
-              );
-            })}
-          </Box>
-        </Box>
       </MainContainer>
     </Box>
   );
