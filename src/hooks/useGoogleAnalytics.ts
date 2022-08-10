2a0560f8bf0ae145aafd6aef05fa742ccb387aca
Agustin Pane <agustin.pane@gmail.com>
2022-08-10 17:11:04 +0200

    Adds useGoogleAnalytics Adds GA_MEASUREMENT_ID in constants file

---
+++ src/hooks/useGoogleAnalytics.ts
@@ -0,0 +1,40 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import { GA_MEASUREMENT_ID } from '@/src/constants'
+
+const pageview = (url: string) => {
+  // @ts-ignore
+  if (window.gtag) {
+    // @ts-ignore
+    window.gtag('config', GA_MEASUREMENT_ID, {
+      page_path: url,
+    })
+  }
+}
+
+export const useGoogleAnalytics = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    return () => {
+      pageview(router.route)
+    };
+  }, [router.route]);
+
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      pageview(url)
+    }
+
+    // When the component is mounted, subscribe to router changes
+    // and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+}
