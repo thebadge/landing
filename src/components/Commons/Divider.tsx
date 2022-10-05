351d96e52a9329a615a0e420a4e0fd089293dd24
Agustin Pane <agustin.pane@gmail.com>
2022-10-05 16:51:48 +0200

    Add custom divider

---
+++ src/components/Commons/Divider.tsx
@@ -0,0 +1,6 @@
+import { Divider, styled } from '@mui/material';
+
+export const CustomDivider = styled(Divider)(({ theme }) => ({
+  borderColor: '#66F5DF',
+  marginBottom: theme.spacing(3),
+}));
