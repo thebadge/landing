c2995f65bda2bdc8d27edc5857650c69868ccb57
Federico Madoery <fede.madoery@hotmail.com>
2022-10-07 10:09:44 -0300

    Add legals on footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,25 +1,26 @@
-import { Box, styled } from '@mui/material';
-import TwitterIcon from '@/src/components/Icons/Twitter';
+import DiscordIcon from '@/src/components/Icons/Discord';
+import EmailIcon from '@/src/components/Icons/Email';
 import GithubIcon from '@/src/components/Icons/Github';
 import MediumIcon from '@/src/components/Icons/Medium';
-import EmailIcon from '@/src/components/Icons/Email';
-import DiscordIcon from '@/src/components/Icons/Discord';
+import TwitterIcon from '@/src/components/Icons/Twitter';
 import {
-  TWITTER_URL,
   DISCORD_URL,
   EMAIL_URL,
   GITHUB_URL,
   MEDIUM_URL,
+  TWITTER_URL,
 } from '@/src/constants';
+import { useIsMobile } from '@/src/hooks/useIsMobile';
+import { Box, Link, styled } from '@mui/material';
+import { CustomDivider } from '../Commons/Divider';
 
 export const FooterContainer = styled(Box)({
   position: 'relative',
   bottom: 0,
-  padding: '5%',
+  padding: '2%',
   display: 'flex',
   flexDirection: 'column',
   background: 'rgba(0, 0, 0, 0.58)',
-  height: 100,
 });
 
 export const SocialContainer = styled(Box)(({ theme }) => ({
@@ -30,7 +31,24 @@ export const SocialContainer = styled(Box)(({ theme }) => ({
   columnGap: theme.spacing(2),
 }));
 
+export const LegalContainer = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  columnGap: theme.spacing(2),
+  color: '#FFF',
+  '& a': {
+    color: '#FFF',
+  },
+  [theme.breakpoints.down('sm')]: {
+    flexDirection: 'column',
+    rowGap: theme.spacing(1),
+  },
+}));
+
 export const Footer = () => {
+  const isMobile = useIsMobile();
+
   return (
     <FooterContainer>
       <SocialContainer>
@@ -40,6 +58,20 @@ export const Footer = () => {
         <EmailIcon link={EMAIL_URL} />
         <DiscordIcon link={DISCORD_URL} />
       </SocialContainer>
+      <CustomDivider sx={{ mx: '25%', marginTop: 2 }} />
+      <LegalContainer>
+        <span>©{new Date().getFullYear()} The Badge</span>
+        <Box sx={{ columnGap: 2, display: 'none' }}>
+          {!isMobile && <span>|</span>}
+          <Link target="_blank">Terms</Link>
+          <span>|</span>
+          <Link target="_blank">Privacy</Link>
+          <span>|</span>
+          <Link target="_blank">Licenses</Link>
+          <span>|</span>
+          <Link target="_blank">Cookie Policy</Link>
+        </Box>
+      </LegalContainer>
     </FooterContainer>
   );
 };
