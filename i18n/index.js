365045213c1aaf20f7051393bcb864240d5f579b
Federico Madoery <fede.madoery@hotmail.com>
2022-12-30 08:52:39 -0300

    Language switcher

--- i18n/index.js
+++ i18n/index.js
@@ -1,8 +1,12 @@
 var en = require('./translations.en.json');
+var es = require('./translations.en.json');
+var de = require('./translations.en.json');
 
 const i18n = {
   translations: {
     en,
+    de,
+    es,
   },
   defaultLang: 'en',
   useBrowserDefault: false,
