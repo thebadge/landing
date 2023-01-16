c2e12975c48ed3ee2e97d5c05bdfd2c8720e2c09
Federico Madoery <fede.madoery@hotmail.com>
2023-01-16 15:48:27 +0000

    Isolate stepper error

--- src/components/BadgeCategories/ThirdParty.tsx
+++ src/components/BadgeCategories/ThirdParty.tsx
@@ -1,9 +1,19 @@
 import { Box, Stack, styled, Typography } from '@mui/material';
 import { useTranslation } from 'next-export-i18n';
+import dynamic from 'next/dynamic';
 import Image from 'next/image';
-import { colors, fonts, SectionLayout, Stepper } from 'thebadge-ui-library';
+import {
+  colors,
+  fonts,
+  SectionLayout,
+  StepperProps,
+} from 'thebadge-ui-library';
 import thirdPartyDecoration from '../../assets/thirdparty-decoration.webp';
 import StyledFirstLetter from '../Commons/StyledFirstLetter';
+
+const Stepper = dynamic<StepperProps>(() =>
+  import('thebadge-ui-library').then((mod) => mod.Stepper),
+);
 
 const StyledSectionLayout = styled(SectionLayout)(({}) => ({
   maxWidth: 'fit-content',
