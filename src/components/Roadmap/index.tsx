e29652f80a9489d20f211ac48d0e370419c61dbd
Federico Madoery <fede.madoery@hotmail.com>
2023-03-22 17:06:26 -0300

    Missing file... Upss

---
+++ src/components/Roadmap/index.tsx
@@ -0,0 +1,8 @@
+import { Roadmap } from 'thebadge-ui-library';
+
+const RoadmapSection = () => {
+  // TODO Add a wrapper to add the needed margins
+  return <Roadmap />;
+};
+
+export default RoadmapSection;
