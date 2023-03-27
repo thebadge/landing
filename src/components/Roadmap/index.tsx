ccf5d3d001203e407183c5a8af163a8fb0959c41
Agustín Longoni <agustin.longoni@altoros.com>
2023-03-27 11:08:50 -0300

    add content wrapper

--- src/components/Roadmap/index.tsx
+++ src/components/Roadmap/index.tsx
@@ -1,8 +1,38 @@
-import { Roadmap } from 'thebadge-ui-library';
+import { colors, Roadmap } from 'thebadge-ui-library';
+import { CustomDivider } from '@/src/components/Commons/Divider';
+import { Box, styled, Typography } from '@mui/material';
+
+const BoxRoadmap = styled(Box)(({ theme }) => ({
+  marginTop: theme.spacing(12),
+  marginBottom: theme.spacing(10),
+}));
 
 const RoadmapSection = () => {
-  // TODO Add a wrapper to add the needed margins
-  return <Roadmap />;
+  return (
+    <BoxRoadmap>
+      <CustomDivider />
+      <Typography
+        variant="h2"
+        component="h3"
+        textAlign="center"
+        color={colors.white}
+      >
+        ROADMAP
+      </Typography>
+      <Box
+        sx={{
+          display: 'flex',
+          flexFlow: 'row wrap',
+          columnGap: 3,
+          rowGap: 8,
+          marginBottom: 4,
+          marginTop: 4,
+        }}
+      >
+        <Roadmap />
+      </Box>      
+    </BoxRoadmap>
+  );
 };
 
 export default RoadmapSection;
