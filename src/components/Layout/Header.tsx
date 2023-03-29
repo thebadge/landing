bb8b2ef35f7877cf5974052e003e678f9d71e6bb
Federico Madoery <fede.madoery@hotmail.com>
2023-03-29 14:07:19 -0300

    Remove un-used code

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -43,7 +43,6 @@ const Header = () => {
     partnershipSection,
     contactSection,
   } = useSetionReferences();
-  // const showMenu = useShowMenu()
   const { t } = useTranslation();
 
   const scrollTo = (sectionRef: RefObject<HTMLDivElement> | null) => {
