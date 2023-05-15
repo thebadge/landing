014092a98bb5687b6ec53690c8f06d0b81bbc25b
Agustin <agustin.pane@gmail.com>
2023-05-15 14:02:51 +0200

    Updates UI Lib version (#84)
    Updates roadmap

--- src/components/Roadmap/index.tsx
+++ src/components/Roadmap/index.tsx
@@ -1,4 +1,4 @@
-import { colors, Roadmap } from 'thebadge-ui-library';
+import { colors, Roadmap, RoadmapItem } from 'thebadge-ui-library';
 import { CustomDivider } from '@/src/components/Commons/Divider';
 import { Box, styled, Typography } from '@mui/material';
 
@@ -6,6 +6,43 @@ const BoxRoadmap = styled(Box)(({ theme }) => ({
   marginTop: theme.spacing(12),
   marginBottom: theme.spacing(10),
 }));
+
+const roadmapElements: RoadmapItem[] = [
+  {
+    numberTitle: '01',
+    quarterNumber: 'Q1 2023',
+    quarterTitle: 'Certification Platform Development',
+    quarterDescription:
+      'Develop core functionalities of the platform, including creation, minting, and curation processes.',
+    quarterDone: true,
+  },
+  {
+    numberTitle: '02',
+    quarterNumber: 'Q2 2023',
+    quarterTitle: 'Beta Launch and Marketing Campaign',
+    quarterDescription:
+      'Start the beta testing program and marketing pre-launch campaign, with incentives for early adopters.',
+  },
+  {
+    numberTitle: '03',
+    quarterNumber: 'Q3 2023',
+    quarterTitle: 'Going Live',
+    quarterDescription: 'We will be releasing our platform.',
+  },
+  {
+    numberTitle: '04',
+    quarterNumber: 'Q4 2023',
+    quarterTitle: 'Third-Party',
+    quarterDescription:
+      'Develop and release the third-party creators functionality for B2B partners.',
+  },
+  {
+    numberTitle: '05',
+    quarterNumber: '2024',
+    quarterTitle: 'Security Layer and More',
+    quarterDescription: 'Integrate private evidence and ZKBadges.',
+  },
+];
 
 const RoadmapSection = () => {
   return (
@@ -29,8 +66,8 @@ const RoadmapSection = () => {
           marginTop: 4,
         }}
       >
-        <Roadmap />
-      </Box>      
+        <Roadmap elements={roadmapElements} />
+      </Box>
     </BoxRoadmap>
   );
 };
